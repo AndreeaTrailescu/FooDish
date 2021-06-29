8b86f38ca5b63c06861c3b0d0bc75619ae7c9700
Andreea Trailescu <andreeatrailescu@gmail.com>
2021-06-29 13:59:16 +0300

    create my menu fxml

---
+++ src/main/java/org/openjfx/controllers/DishItemController.java
@@ -0,0 +1,17 @@
+package org.openjfx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class DishItemController {
+
+    @FXML
+    private Label dishName;
+    @FXML
+    private Label dishPrice;
+    @FXML
+    private ImageView dishImage;
+
+
+}
---
+++ src/main/java/org/openjfx/controllers/MyMenuController.java
@@ -0,0 +1,28 @@
+package org.openjfx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class MyMenuController {
+
+    @FXML
+    private Button back;
+    @FXML
+    private VBox chooseDish;
+    @FXML
+    private Label dishName;
+    @FXML
+    private Label dishPrice;
+    @FXML
+    private ImageView dishImage;
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private GridPane gridDishes;
+
+}
